02f4dc1cae61a0f677d726d74e23d7dca02af4c9
Seema Vora <seema.s.vora@gmail.com>
2021-03-07 10:57:47 -0800

    Adds All Functionalities

--- Project1/src/cs146S21/vora/project1/LinkedList.java
+++ Project1/src/cs146S21/vora/project1/LinkedList.java
@@ -81,25 +81,61 @@ public class LinkedList {
 		System.out.println("This item does not exist.");
 	}
 
-	public void checkItemExists(String data) {
-		Node current = head;
+	public Boolean checkItemExists(String data, LinkedList A) {
+		Node current = A.head;
 		Boolean check = false;
-		if (head == null) {
+		if (A.head == null) {
 			System.out.println("This list is empty.");
-			return;
+			return false;
 		}
 		while (current.next != null) {
 			current = current.next;
-			if (current.data == data || head.data == data) {
+			if (current.data == data || A.head.data == data) {
 				check = true;
 			}
 		}
 		if (check == true) {
 			System.out.println("The value: " + data + " exists in the list");
+			return check;
 		}else {
 			System.out.println("The value: " + data + " does not exist in the list");
+			return check;
 		}
 		
+	}
+	
+	public void listUnion (LinkedList list1, LinkedList list2) {
+		// gather all distinct elements of a list
+		// Since it is a set all items already only occur once in each list
+		Node current1 = list1.head;
+		Node current2 = list2.head;
+		while (current1 != null) {
+			insertItem(current1.data);
+			current1 = current1.next;
+		}
+		
+		while (current2 != null) {
+			Boolean check = checkItemExists(current2.data, list1);
+			if(check == false) {
+				insertItem(current2.data);
+			}
+			current2 = current2.next;
+			
+		}
+	}
+	
+	public void listIntersection (LinkedList list1, LinkedList list2) {
+		Node current2 = list2.head;
+		
+		
+		while (current2 != null) {
+			Boolean check = checkItemExists(current2.data, list1);
+			if(check == true) {
+				insertItem(current2.data);
+			}
+			current2 = current2.next;
+			
+		}
 	}
 	
 	
@@ -123,14 +159,35 @@ public class LinkedList {
 
 	public static void main(String[] args) {
 		LinkedList A = new LinkedList();
+		LinkedList B = new LinkedList();
+		LinkedList C = new LinkedList();
+		LinkedList D = new LinkedList();
 		A.insertItem("hi");
 		A.insertItem("hello");
-		A.display();
-		A.returnRef("Nanar");
+		A.insertItem("there");
+		A.insertItem("eat");
+		A.insertItem("banana");
 		A.insertItem("bye");
 		A.insertItem("Nanar");
-		A.insertItem("Seema");
+		A.checkItemExists("hi", A);
+		B.insertItem("hi");
+		B.insertItem("cat");
+		B.insertItem("there");
+		B.insertItem("eat");
+		B.insertItem("banana");
+		B.insertItem("bye");
+		B.insertItem("Nanar");
+		B.insertItem("Seema");
+		C.listUnion(A, B);
+		D.listIntersection(A,B);
+		System.out.println(" ");
 		A.display();
+		System.out.println(" ");
+		B.display();
+		System.out.println(" ");
+		C.display();
+		System.out.println("D: ");
+		D.display();
 
 	}
 
